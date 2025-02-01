5a70d24bc447f3c12e8f1c4e356385055749d304
observerUA <kirillkuznetsov181@gmail.com>
2025-02-01 04:12:23 +0200

    generationValue = sequence

--- src/main/resources/db/migration/V5__create_global_sequence.sql
+++ src/main/resources/db/migration/V5__create_global_sequence.sql
@@ -1,10 +1,17 @@
 drop sequence if exists global_id_seq cascade;
 
-create sequence global_sequence start with -2147483648 increment by 1 no cycle;
+create sequence global_sequence
+    start with -2147483648
+    increment by 1
+    minvalue -2147483648
+    no cycle;
 
-alter table permissions alter column id set default nextval('global_sequence');
-alter table roles alter column id set default nextval('global_sequence');
-alter table users alter column id set default nextval('global_sequence');
+alter table permissions
+    alter column id set default nextval('global_sequence');
+alter table roles
+    alter column id set default nextval('global_sequence');
+alter table users
+    alter column id set default nextval('global_sequence');
 
 drop sequence if exists permissions_id_seq;
 drop sequence if exists roles_id_seq;
